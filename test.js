aa0b74ee28847ce7157f962d1eb49122a286a9b5
fanbin <2582620878@qq.com>
2023-07-04 16:41:30 +0800

    feat: 添加test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: fanbin 2582620878@qq.com
+ * @Date: 2023-07-04 16:41:11
+ * @LastEditors: fanbin 2582620878@qq.com
+ * @LastEditTime: 2023-07-04 16:41:17
+ * @FilePath: /gittest/test.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('123')
